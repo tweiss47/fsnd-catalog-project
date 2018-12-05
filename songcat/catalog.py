2b54328d61f842dd4a20576844320dee7eca1b18
Taylor Weiss <taylor.weiss@comcast.net>
2018-12-04 18:07:54 -0800

    Index template, styles and stub model

--- songcat/catalog.py
+++ songcat/catalog.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+from . import model
 
 
 bp = Blueprint('catalog', __name__)
@@ -8,17 +9,27 @@ bp = Blueprint('catalog', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('base.html')
+    genres = [
+        'Blues', 'Classical', 'Jazz', 'Reggae', 'Country',
+        'Electronic', 'Folk', 'Rock', 'Soul/R&B'
+    ]
+    songs = [
+        model.Song(1, 'Across the Tappan Zee', 'Folk'),
+        model.Song(2, 'All Day Sucker', 'Soul/R&B'),
+        model.Song(3, 'La Grange', 'Blues'),
+        model.Song(4, 'Little Maggie', 'Folk')
+    ]
+    return render_template('catalog/index.html', genres=genres, songs=songs)
 
 
-@bp.route('/genre/<name>')
-def genre(name):
-    return 'genre index'
+@bp.route('/genre/<path:name>')
+def genre_view(name):
+    return 'genre view for {}'.format(name)
 
 
 @bp.route('/song/<int:id>')
 def song_view(id):
-    return 'song view'
+    return 'song view for {}'.format(id)
 
 
 @bp.route('/song/add', methods=('GET', 'POST'))
---
+++ songcat/model.py
@@ -0,0 +1,8 @@
+
+
+class Song:
+    def __init__(self, id, title, genre):
+        self.id = id
+        self.title = title
+        self.genre = genre
+
