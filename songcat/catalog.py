66c35a742746ae8f2857b980973df16395bfa8a7
Taylor Weiss <taylor.weiss@comcast.net>
2018-12-04 18:35:23 -0800

    Genre view

--- songcat/catalog.py
+++ songcat/catalog.py
@@ -9,22 +9,17 @@ bp = Blueprint('catalog', __name__)
 
 @bp.route('/')
 def index():
-    genres = [
-        'Blues', 'Classical', 'Jazz', 'Reggae', 'Country',
-        'Electronic', 'Folk', 'Rock', 'Soul/R&B'
-    ]
-    songs = [
-        model.Song(1, 'Across the Tappan Zee', 'Folk'),
-        model.Song(2, 'All Day Sucker', 'Soul/R&B'),
-        model.Song(3, 'La Grange', 'Blues'),
-        model.Song(4, 'Little Maggie', 'Folk')
-    ]
-    return render_template('catalog/index.html', genres=genres, songs=songs)
+    return render_template('catalog/index.html', genres=model.genres, songs=model.songs)
 
 
 @bp.route('/genre/<path:name>')
 def genre_view(name):
-    return 'genre view for {}'.format(name)
+    return render_template(
+        'catalog/genre_view.html',
+        genres=model.genres,
+        selected=name,
+        songs=model.songs_for(name)
+    )
 
 
 @bp.route('/song/<int:id>')
--- songcat/model.py
+++ songcat/model.py
@@ -6,3 +6,19 @@ class Song:
         self.title = title
         self.genre = genre
 
+
+genres = [
+    'Blues', 'Classical', 'Jazz', 'Reggae', 'Country',
+    'Electronic', 'Folk', 'Rock', 'Soul/R&B'
+]
+songs = [
+    Song(1, 'Across the Tappan Zee', 'Folk'),
+    Song(2, 'All Day Sucker', 'Soul/R&B'),
+    Song(3, 'La Grange', 'Blues'),
+    Song(4, 'Little Maggie', 'Folk')
+]
+
+
+def songs_for(genre):
+    return [s for s in songs if s.genre == genre]
+
